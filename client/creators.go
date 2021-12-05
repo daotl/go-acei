tendermint/tendermint

libs/service: pass logger explicitly (#7288)
This is a very small change, but removes a method from the
`service.Service` interface (a win!) and forces callers to explicitly
pass loggers in to objects during construction rather than (later)
injecting them. There's not a real need for this kind of lazy
construction of loggers, and I think a decent potential for confusion
for mutable loggers.

The main concern I have is that this changes the constructor API for
ABCI clients. I think this is fine, and I suspect that as we plumb
contexts through, and make changes to the RPC services there'll be a
number of similar sorts of changes to various (quasi) public
interfaces, which I think we should welcome.

abci/client/creators.go
@@ -5,17 +5,18 @@ import (
 
 	"github.com/tendermint/tendermint/abci/types"
 	tmsync "github.com/tendermint/tendermint/internal/libs/sync"
+	"github.com/tendermint/tendermint/libs/log"
 )
 
 // Creator creates new ABCI clients.
-type Creator func() (Client, error)
+type Creator func(log.Logger) (Client, error)
 
 // NewLocalCreator returns a Creator for the given app,
 // which will be running locally.
 func NewLocalCreator(app types.Application) Creator {
 	mtx := new(tmsync.Mutex)
 
-	return func() (Client, error) {
+	return func(_ log.Logger) (Client, error) {
 		return NewLocalClient(mtx, app), nil
 	}
 }
@@ -23,9 +24,9 @@ func NewLocalCreator(app types.Application) Creator {
 // NewRemoteCreator returns a Creator for the given address (e.g.
 // "192.168.0.1") and transport (e.g. "tcp"). Set mustConnect to true if you
 // want the client to connect before reporting success.
-func NewRemoteCreator(addr, transport string, mustConnect bool) Creator {
-	return func() (Client, error) {
-		remoteApp, err := NewClient(addr, transport, mustConnect)
+func NewRemoteCreator(logger log.Logger, addr, transport string, mustConnect bool) Creator {
+	return func(log.Logger) (Client, error) {
+		remoteApp, err := NewClient(logger, addr, transport, mustConnect)
 		if err != nil {
 			return nil, fmt.Errorf("failed to connect to proxy: %w", err)
 		}

daotl/go-acei

client/creators.go
@@ -9,43 +9,25 @@ import (
 	"github.com/daotl/go-acei/types"
 )
 
-// LoggerCreator creates new loggers.
-type LoggerCreator func() (log.StandardLogger, error)
-
 // Creator creates new ACEI clients.
-type Creator func() (Client, error)
+type Creator func(log.StandardLogger) (Client, error)
 
 // NewLocalCreator returns a Creator for the given app,
 // which will be running locally.
-func NewLocalCreator(app types.Application, loggerCreator LoggerCreator) Creator {
+func NewLocalCreator(logger log.StandardLogger, app types.Application) Creator {
 	mtx := new(gsync.Mutex)
 
-	return func() (Client, error) {
-		var logger log.StandardLogger = nil
-		if loggerCreator != nil {
-			var err error
-			if logger, err = loggerCreator(); err != nil {
-				return nil, err
-			}
-		}
-		return NewLocalClient(mtx, app, logger)
+	return func(log.StandardLogger) (Client, error) {
+		return NewLocalClient(logger, mtx, app)
 	}
 }
 
 // NewRemoteCreator returns a Creator for the given address (e.g.
 // "192.168.0.1") and transport (e.g. "tcp"). Set mustConnect to true if you
 // want the client to connect before reporting success.
-func NewRemoteCreator(addr, transport string, mustConnect bool, loggerCreator LoggerCreator) Creator {
-	return func() (Client, error) {
-		var logger log.StandardLogger = nil
-		if loggerCreator != nil {
-			var err error
-			if logger, err = loggerCreator(); err != nil {
-				return nil, err
-			}
-		}
-
-		remoteApp, err := NewClient(addr, transport, mustConnect, logger)
+func NewRemoteCreator(logger log.StandardLogger, addr, transport string, mustConnect bool) Creator {
+	return func(log.StandardLogger) (Client, error) {
+		remoteApp, err := NewClient(logger, addr, transport, mustConnect)
 		if err != nil {
 			return nil, fmt.Errorf("failed to connect to proxy: %w", err)
 		}